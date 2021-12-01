ea6aed30c10070e3182293e2176f0493ff3f5155
Cameron Rosenberger <crosenberger17@gmail.com>
2021-12-01 11:05:53 -0800

    added test cases for jest

--- day01.js
+++ day01.js
@@ -1,4 +1,11 @@
-function sum(a, b) {
+const { getArrayFromFile } = require("./day01array");
+const { countDepthIncreases } = require("./day01CountIncreases");
+
+const depths = getArrayFromFile("./day01inputs.txt");
+
+test("Question 1: ", countDepthIncreases(depths));
+
+/* function sum(a, b) {
   return a + b;
 }
-module.exports = sum;
+module.exports = sum; */
---
+++ day01CountIncreases.js
@@ -0,0 +1,25 @@
+function countDepthIncreases(depths) {
+  // ensure input is valid
+  if (depths.length < 2) {
+    return 0;
+  }
+
+  let deeperCount = 0;
+  let previousDepth = depths[0];
+
+  for (let i = 1; i < depths.length; i++) {
+    const currentDepth = depths[i];
+
+    if (currentDepth > previousDepth) {
+      deeperCount++;
+    }
+
+    previousDepth = currentDepth;
+  }
+
+  return deeperCount;
+}
+
+module.exports = {
+  countDepthIncreases
+};
